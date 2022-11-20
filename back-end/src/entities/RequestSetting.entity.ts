8c9811d40d11dfd5f8cc5efeafbe21bf3e799161
Vianney Accart <vianneyaccart@gmail.com>
2022-11-20 19:15:06 +0100

    Remove useless isAgreedTerms property from User.entity, create RequestSetting entity and repository

---
+++ back-end/src/entities/RequestSetting.entity.ts
@@ -0,0 +1,77 @@
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Field, ID, ObjectType } from 'type-graphql';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import User from './User.entity';
+
+export enum Frequency {
+  THIRTY_DAYS = 2592000,
+  SEVEN_DAYS = 604800,
+  ONE_DAY = 86400,
+  TWELVE_HOURS = 43200,
+  SIX_HOURS = 21600,
+  ONE_HOUR = 3600,
+  THIRTY_MINUTES = 1800,
+  FIFTEEN_MINUTES = 900,
+  ONE_MINUTE = 60,
+  THIRTY_SECONDS = 30,
+  FIFTEEN_SECONDS = 15,
+  FIVE_SECONDS = 5,
+}
+
+@Entity("request_setting")
+@ObjectType()
+export default class RequestSetting {
+  constructor(
+    user: User,
+    url: string,
+    frequency: number,
+    isActive: boolean,
+    name?: string | any, // Can't set to string | undefined because of TypeORM
+    headers?: JSON
+  ) {
+    this.user = user;
+    this.frequency = frequency;
+    this.url = url;
+    this.isActive = isActive;
+    this.name = name;
+    this.headers = JSON.stringify(headers);
+  }
+
+  @PrimaryGeneratedColumn("uuid")
+  @Field(() => ID)
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @ManyToOne(() => User, { eager: true })
+  user: User;
+
+  @Column()
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @Column({ type: "enum", enum: Frequency, default: Frequency.ONE_HOUR })
+  @Field()
+  @IsNotEmpty()
+  frequency: Frequency;
+
+  @Column({ default: true })
+  @Field()
+  @IsNotEmpty()
+  @IsBoolean()
+  isActive: boolean;
+
+  @Column({ nullable: true, default: null })
+  @Field({ nullable: true })
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @Column({ nullable: true, default: null })
+  @Field({ nullable: true })
+  @IsString()
+  headers: string;
+}
