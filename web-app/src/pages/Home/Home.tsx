ce6f90f2d28243bb1bcb0dab1e8af4c3e70ac4cb
Vianney Accart <vianneyaccart@gmail.com>
2022-11-24 20:37:00 +0100

    Create some global scss variables, begin search bar container

--- web-app/src/pages/Home/Home.tsx
+++ web-app/src/pages/Home/Home.tsx
@@ -3,8 +3,22 @@ import styles from './Home.module.scss';
 const Home = () => {
   return (
     <>
-      <div className="container d-flex justify-content-center">
-        <h1 className={styles.title}>Accueil</h1>
+      <div className={styles.searchBarContainer}>
+        <h1 className="col-5">
+          Enter a website URL and check its availability
+        </h1>
+        <div className="col-6">
+          <form>
+            <input type="text" />
+          </form>
+        </div>
+      </div>
+      <div className="container">
+        <h1 className={styles.title}>How it works</h1>
+        <p className={styles.text}>
+          HealthCheck allows you to test if a website is operational by sending
+          a request and analyzing the response.
+        </p>
       </div>
     </>
   );
