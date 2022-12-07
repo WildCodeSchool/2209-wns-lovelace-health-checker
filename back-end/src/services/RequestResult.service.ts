56cb25e43c37d569038ffe574728d6a5b75bc93f
Ario Ngu <daitenshifuron@gmail.com>
2022-12-07 17:51:23 +0100

    create service for request

---
+++ back-end/src/services/RequestResult.service.ts
@@ -0,0 +1,25 @@
+import { HomepageRequestResult } from '../models/requestResult.model';
+import RequestResultRepository from '../repositories/RequestResult.repository';
+
+export default class RequestResultService extends RequestResultRepository {
+  static checkUrl(url: string): HomepageRequestResult {
+    let isAvailable: boolean;
+    let statusCode: number;
+    let duration: number;
+    if (Math.floor(Math.random() * 2) == 0) {
+        isAvailable = true;
+        statusCode = 200;
+      } else {
+        isAvailable = false;
+        statusCode = 404;
+      }
+      duration = Math.floor(Math.random() * 9001);
+  
+      const homepageRequestResult: HomepageRequestResult = {
+        isAvailable,
+        statusCode,
+        duration,
+      };
+    return homepageRequestResult;
+  }
+}
