43492cb899a15cc0fbf42a58cab49211c25a3ed5
Vianney Accart <vianneyaccart@gmail.com>
2023-01-25 17:34:36 +0100

    End ForgotPassword component

--- web-app/src/pages/ForgotPassword/ForgotPassword.tsx
+++ web-app/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -15,36 +15,25 @@ export const ASK_FOR_NEW_PASSWORD = gql`
 `;
 
 const ForgotPassword = () => {
-  const [askForNewPassword, { data, loading }] = useMutation<
+  const [askForNewPassword, { loading }] = useMutation<
     AskForNewPasswordMutation,
     AskForNewPasswordMutationVariables
   >(ASK_FOR_NEW_PASSWORD, {
-    onError: (error) => {
-      switch (error.message) {
-        case "Argument Validation Error":
-          toast.error(
-            "Unable to create your account because the registration form contains one or more invalid fields.",
-            {
-              position: toast.POSITION.BOTTOM_RIGHT,
-              toastId: 1,
-            }
-          );
-          break;
-        case "This email is already used":
-          toast.error("This email is already used", {
-            position: toast.POSITION.BOTTOM_RIGHT,
-            toastId: 2,
-          });
-          break;
-        default:
-          toast.error(
-            "Oops... It seems that something went wront, please try again.",
-            {
-              position: toast.POSITION.BOTTOM_RIGHT,
-              toastId: 3,
-            }
-          );
-      }
+    onCompleted: (data) => {
+      reset();
+      toast.success(data.askForNewPassword, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        toastId: 3,
+      });
+    },
+    onError: () => {
+      toast.error(
+        "Oops... It seems that something went wront, please try again.",
+        {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          toastId: 3,
+        }
+      );
     },
   });
 
@@ -52,6 +41,7 @@ const ForgotPassword = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<any>({
     criteriaMode: "all",
   });
@@ -65,47 +55,52 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div data-testid="formContainer" className={styles.desktopContainer}>
+    <div className={styles.contentContainer}>
       <h1 className={`mb-4`}>It happens even to the best...</h1>
-      <p>
-        Enter your email address and you will receive a link to generate a new
-        password.
-      </p>
-      <form className={styles.signUpForm} onSubmit={handleSubmit(onSubmit)}>
-        {loading ? (
-          <div
-            className={`${styles.loaderContainer} d-flex justify-content-center align-items-center`}
-          >
-            <div className={styles.loader} role="status"></div>
-          </div>
-        ) : (
-          <></>
-        )}
-
-        <div className="form-floating mb-2">
-          <input
-            type="email"
-            defaultValue={""}
-            className="form-control"
-            {...register("email", {
-              required: "Email is required",
-            })}
-            id="email"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="email">Email</label>
-        </div>
-        <div className={styles.errorMessage}>
-          <FormErrorMessage errors={errors} name={"email"} />
-        </div>
-
-        <button
-          type="submit"
-          className={`${styles.button} ${styles.primaryButton} mt-4`}
+      <div data-testid="formContainer" className={styles.desktopContainer}>
+        <p>
+          Enter your email address and you will receive a link to generate a new
+          password.
+        </p>
+        <form
+          className={styles.askForNewPasswordForm}
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Create your account
-        </button>
-      </form>
+          {loading ? (
+            <div
+              className={`${styles.loaderContainer} d-flex justify-content-center align-items-center`}
+            >
+              <div className={styles.loader} role="status"></div>
+            </div>
+          ) : (
+            <></>
+          )}
+
+          <div className="form-floating mb-2">
+            <input
+              type="email"
+              defaultValue={""}
+              className="form-control"
+              {...register("email", {
+                required: "Email is required",
+              })}
+              id="email"
+              placeholder="name@example.com"
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className={styles.errorMessage}>
+            <FormErrorMessage errors={errors} name={"email"} />
+          </div>
+
+          <button
+            type="submit"
+            className={`${styles.button} ${styles.primaryButton} mt-4`}
+          >
+            Confirm
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
