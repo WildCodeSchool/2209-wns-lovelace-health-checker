ac8131c7dd091464f87c64a412ee1b12e80ee47c
Ario Ngu <ario.ngu@gmail.com>
2023-01-25 15:40:52 +0100

    url args for check url request is verified with validator

--- back-end/src/resolvers/RequestResult/RequestResult.input.ts
+++ back-end/src/resolvers/RequestResult/RequestResult.input.ts
@@ -1,15 +1,13 @@
 import { ArgsType, Field } from "type-graphql";
+import { Matches } from "class-validator";
 
-// Minimum eight characters, at least one uppercase letter, one lowercase letter and one number, for special character, add " (?=.*[*.!@$%^&(){}[]:;<>/,.?~_+-=|\]) "
-const passwordRegExp = new RegExp("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$");
-
-// Support international names with super sweet unicode
-const regExp = RegExp(
-  /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+const urlRegExp = RegExp(
+  /^(http|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/
 );
 
 @ArgsType()
 export class checkUrlArgs {
   @Field()
+  @Matches(urlRegExp, { message: "Invalid URL" })
   url: string;
 }
