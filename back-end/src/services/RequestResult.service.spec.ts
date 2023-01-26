2a91605fcd16c1d7f9026cbcb71c0da28211e111
Ario Ngu <ario.ngu@gmail.com>
2023-01-26 15:38:07 +0100

    deleted comments on test

--- back-end/src/services/RequestResult.service.spec.ts
+++ back-end/src/services/RequestResult.service.spec.ts
@@ -2,28 +2,13 @@ import RequestResultService from "./RequestResult.service";
 import RequestResult from "../entities/RequestResult.entity";
 import { closeConnection, initializeRepositories } from "../database/utils";
 
-// jest.mock("node-fetch", () =>
-//   jest.fn(() =>
-//     Promise.resolve({
-//       ok: true,
-//       status: 200,
-//       json: () => Promise.resolve({}),
-//     })
-//   )
-// );
-
-//jest.mock("node-fetch", () => jest.fn());
-
 describe("RequestResultService", () => {
   beforeAll(async () => {
     await initializeRepositories();
-    // await RequestResultService.initializeRepository();
   });
 
   afterEach(async () => {
     await RequestResultService.clearRepository();
-    // jest.resetAllMocks();
-    // jest.clearAllMocks();
   });
 
   afterAll(async () => {
@@ -31,7 +16,7 @@ describe("RequestResultService", () => {
   });
 
   describe("checkUrl", () => {
-    it("should return a RequestResult object with a 200 status code and the correct response time", async () => {
+    it("should return a RequestResult object with a 200 status code", async () => {
       const url = "https://www.youtube.com";
 
       jest.mock("node-fetch", () =>
@@ -44,13 +29,10 @@ describe("RequestResultService", () => {
         )
       );
 
-      // const startTimer = Date.now();
       const requestResult = await RequestResultService.checkUrl(url);
-      // const endTimer = Date.now();
 
       expect(requestResult).toBeInstanceOf(RequestResult);
       expect(requestResult.statusCode).toEqual(200);
-      // expect(requestResult.duration).toEqual(endTimer - startTimer);
     });
 
     it("should throw an error if the request times out", async () => {
@@ -71,14 +53,6 @@ describe("RequestResultService", () => {
       );
     });
 
-    // it("should throw an error if the fetch fails", async () => {
-    //   const url = "http://invalid-url.com";
-
-    //   await expect(RequestResultService.checkUrl(url)).rejects.toThrowError(
-    //     "Fetch Failed"
-    //   );
-    // });
-
     it("should throw an error if the fetch fails", async () => {
       // Use a mock implementation of the fetch method that always throws an error
       global.fetch = jest.fn(() => {
@@ -86,9 +60,8 @@ describe("RequestResultService", () => {
       });
 
       const url = "https://example.com";
-      const timeout = 5000;
 
-      await expect(RequestResultService.checkUrl(url, timeout)).rejects.toThrow(
+      await expect(RequestResultService.checkUrl(url)).rejects.toThrow(
         "Fetch Failed"
       );
     });
