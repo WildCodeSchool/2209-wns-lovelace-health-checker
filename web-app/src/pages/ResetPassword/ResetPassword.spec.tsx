1543713b552432f3cd4f4feb95c7dfe8cb804151
Vianney Accart <vianneyaccart@gmail.com>
2023-01-27 12:30:23 +0100

    End ResetPassword tests

--- web-app/src/pages/ResetPassword/ResetPassword.spec.tsx
+++ web-app/src/pages/ResetPassword/ResetPassword.spec.tsx
@@ -2,27 +2,18 @@ import "@testing-library/jest-dom";
 
 import { MockedProvider, MockedResponse } from "@apollo/client/testing";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { createMemoryHistory, MemoryHistory } from "history";
-import { MemoryRouter, Route, Router, Routes } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import * as toastify from "react-toastify";
+import * as router from "react-router";
 
 import { ResetPasswordMutation } from "../../gql/graphql";
 import { SERVER_IS_KO_ERROR_MESSAGE } from "../../utils/error-messages";
 import ResetPassword, { RESET_PASSWORD } from "./ResetPassword";
 
 jest.mock("react-toastify");
+const navigate = jest.fn();
 
-const renderResetPassword = (mock?: any) => {
-  render(
-    <MockedProvider mocks={mock}>
-      <MemoryRouter>
-        <ResetPassword />
-      </MemoryRouter>
-    </MockedProvider>
-  );
-};
-
-const renderResetPasswordWithToken = (token: string, mock?: any) => {
+const renderResetPassword = (token: string, mock?: any) => {
   render(
     <MockedProvider mocks={mock}>
       <MemoryRouter initialEntries={[`/reset-password/${token}`]}>
@@ -37,16 +28,6 @@ const renderResetPasswordWithToken = (token: string, mock?: any) => {
   );
 };
 
-const renderResetPasswordWithHistory = (history: MemoryHistory, mock?: any) => {
-  render(
-    <MockedProvider mocks={mock}>
-      <Router location={history.location} navigator={history}>
-        <ResetPassword />
-      </Router>
-    </MockedProvider>
-  );
-};
-
 const VALID_TOKEN = "valid-token";
 const INVALID_TOKEN = "invalid-token";
 
@@ -57,6 +38,7 @@ const submitForm = (password: string, passwordConfirmation: string) => {
   fireEvent.change(screen.getByTestId("passwordConfirmation"), {
     target: { value: passwordConfirmation },
   });
+  fireEvent.click(screen.getByRole("button", { name: "Reset your password" }));
 };
 
 const RESET_PASSWORD_SUCCESS: MockedResponse<ResetPasswordMutation> = {
@@ -64,8 +46,8 @@ const RESET_PASSWORD_SUCCESS: MockedResponse<ResetPasswordMutation> = {
     query: RESET_PASSWORD,
     variables: {
       token: VALID_TOKEN,
-      password: "password",
-      passwordConfirmation: "password",
+      password: "Vianney13",
+      passwordConfirmation: "Vianney13",
     },
   },
   result: {
@@ -75,13 +57,25 @@ const RESET_PASSWORD_SUCCESS: MockedResponse<ResetPasswordMutation> = {
   },
 };
 
+const RESET_PASSWORD_ERROR: MockedResponse<ResetPasswordMutation> = {
+  request: {
+    query: RESET_PASSWORD,
+    variables: {
+      token: INVALID_TOKEN,
+      password: "Vianney13",
+      passwordConfirmation: "Vianney13",
+    },
+  },
+  error: new Error("Your reset password token is no longer valid"),
+};
+
 const SERVER_IS_KO: MockedResponse<ResetPasswordMutation> = {
   request: {
     query: RESET_PASSWORD,
     variables: {
       token: VALID_TOKEN,
-      password: "password",
-      passwordConfirmation: "password",
+      password: "Vianney13",
+      passwordConfirmation: "Vianney13",
     },
   },
   error: new Error(),
@@ -89,20 +83,20 @@ const SERVER_IS_KO: MockedResponse<ResetPasswordMutation> = {
 
 describe("ResetPassword", () => {
   it("should renders", async () => {
-    renderResetPasswordWithToken(VALID_TOKEN);
+    renderResetPassword(VALID_TOKEN);
   });
   describe("after form submission", () => {
     it("renders a loader awaiting response", async () => {
-      renderResetPasswordWithToken(VALID_TOKEN, [RESET_PASSWORD_SUCCESS]);
-      submitForm("password", "password");
+      renderResetPassword(VALID_TOKEN, [RESET_PASSWORD_SUCCESS]);
+      submitForm("Vianney13", "Vianney13");
       await waitFor(() => {
         expect(screen.getByRole("status")).toBeInTheDocument();
       });
     });
     describe("when request responds with status 200", () => {
       it("renders a success toast with informative message", async () => {
-        renderResetPasswordWithToken(VALID_TOKEN, [RESET_PASSWORD_SUCCESS]);
-        submitForm("password", "password");
+        renderResetPassword(VALID_TOKEN, [RESET_PASSWORD_SUCCESS]);
+        submitForm("Vianney13", "Vianney13");
         await waitFor(() => {
           expect(toastify.toast.success).toHaveBeenCalledWith(
             "Your password has been updated successfully",
@@ -110,19 +104,31 @@ describe("ResetPassword", () => {
           );
         });
       });
-      it("redirects to sign in", async () => {
-        const history = createMemoryHistory();
-        renderResetPasswordWithHistory(history, [RESET_PASSWORD_SUCCESS]);
-        submitForm("password", "password");
+      it('should call navigate with "/sign-in"', async () => {
+        jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+        renderResetPassword(VALID_TOKEN, [RESET_PASSWORD_SUCCESS]);
+        submitForm("Vianney13", "Vianney13");
         await waitFor(() => {
-          expect(history.location.pathname).toBe("/sign-in");
+          expect(navigate).toHaveBeenCalledWith("/sign-in");
+        });
+      });
+    });
+    describe("when token is no longer valid", () => {
+      it("renders an error toast with 'Your reset password token is no longer valid'", async () => {
+        renderResetPassword(INVALID_TOKEN, [RESET_PASSWORD_ERROR]);
+        submitForm("Vianney13", "Vianney13");
+        await waitFor(() => {
+          expect(toastify.toast.error).toHaveBeenCalledWith(
+            "Your reset password token is no longer valid",
+            { position: toastify.toast.POSITION.BOTTOM_RIGHT, toastId: 4 }
+          );
         });
       });
     });
     describe("server is KO", () => {
       it("renders an error toast", async () => {
-        renderResetPasswordWithToken(VALID_TOKEN, [SERVER_IS_KO]);
-        submitForm("password", "password");
+        renderResetPassword(VALID_TOKEN, [SERVER_IS_KO]);
+        submitForm("Vianney13", "Vianney13");
         await waitFor(() => {
           expect(toastify.toast.error).toHaveBeenCalledWith(
             SERVER_IS_KO_ERROR_MESSAGE,
