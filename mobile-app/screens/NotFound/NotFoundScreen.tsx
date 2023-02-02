289caed9e772a27f9453877c59482056b665412d
Ario Ngu <ario.ngu@gmail.com>
2023-02-02 12:35:09 +0100

    CHANGED style for NotFound is in another file

--- mobile-app/screens/NotFound/NotFoundScreen.tsx
+++ mobile-app/screens/NotFound/NotFoundScreen.tsx
@@ -2,6 +2,7 @@ import { Link } from "@react-navigation/native";
 import React from "react";
 import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import components from "../../components";
+import { styles } from "./NotFoundStyle";
 
 import variables from "../../variables";
 
@@ -26,33 +27,5 @@ const NotFoundScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  
-  bsPMarginBottom: {
-    marginBottom: 16
-  },
-  bold: {
-    fontWeight: "600", // variabiliser
-  },
-  contentContainer: {
-    padding: 24, // variabiliser
-  },
-  btn: {
-    height: 60,
-    borderRadius: 5,
-    marginTop: 16,
-    alignItems: "center", // align horizontally
-    justifyContent: "center" // align vertically
-  }, // variabiliser
-  btnPrimary: {
-    backgroundColor: "#195078", // variabiliser
-    border: "none", // variabiliser
-  },
-  btnText: {
-    color: "white", // variabiliser
-    fontSize: 16, // trouver où le mettre dans l'app
-  },
-});
 
 export default NotFoundScreen;
