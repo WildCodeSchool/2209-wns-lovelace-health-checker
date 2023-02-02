a53914214d1a9bdf17fad9bc1ebbd0a6f1feb52d
Ario Ngu <ario.ngu@gmail.com>
2023-02-02 12:33:01 +0100

    ADDED REQUEST_TIMEOUT env variable

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 
-console.log("env", process.env.GRAPHQL_API_URL);
-
 export default {
   extra: {
+    REQUEST_TIMEOUT: process.env.REQUEST_TIMEOUT,
     GRAPHQL_API_URL: process.env.GRAPHQL_API_URL,
   },
 };
