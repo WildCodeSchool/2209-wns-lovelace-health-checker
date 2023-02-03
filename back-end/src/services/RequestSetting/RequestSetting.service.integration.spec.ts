e3c1728272eb6fea482a5d5a0eae5de9e1f2c5d7
Vianney Accart <vianneyaccart@gmail.com>
2023-02-03 13:08:14 +0100

    Add some tests for RequestSetting.service

--- back-end/src/services/RequestSetting/RequestSetting.service.integration.spec.ts
+++ back-end/src/services/RequestSetting/RequestSetting.service.integration.spec.ts
@@ -1,12 +1,182 @@
+import {
+  closeConnection,
+  getDatabase,
+  initializeRepositories,
+  truncateAllTables,
+} from "../../database/utils";
+import RequestSetting, {
+  Frequency,
+} from "../../entities/RequestSetting.entity";
+import User, { Role } from "../../entities/User.entity";
+import AlertSettingService from "../AlertSetting/AlertSetting.service";
+import RequestSettingService from "./RequestSetting.service";
+
+const setPushAlerts = () => {
+  return jest
+    .spyOn(AlertSettingService, "setPushAlerts")
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
+const setEmailAlerts = () => {
+  return jest
+    .spyOn(AlertSettingService, "setEmailAlerts")
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
+const checkIfHeadersAreRightFormatted = () => {
+  return jest
+    .spyOn(RequestSettingService, "checkIfHeadersAreRightFormatted")
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
+const checkIfNonPremiumUserTryToUsePremiumFrequency = () => {
+  return jest
+    .spyOn(
+      RequestSettingService,
+      "checkIfNonPremiumUserTryToUsePremiumFrequency"
+    )
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
+const checkIfNonPremiumUserTryToUseCustomError = () => {
+  return jest
+    .spyOn(RequestSettingService, "checkIfNonPremiumUserTryToUseCustomError")
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
+const createRequestSetting = () => {
+  return jest
+    .spyOn(RequestSettingService, "createRequestSetting")
+    .mockImplementation((data: any) => {
+      return data;
+    });
+};
+
 describe("RequestService integration", () => {
+  const userWithUserRole = new User(
+    "John",
+    "Doe",
+    "john.doe@email.com",
+    "password"
+  );
+  userWithUserRole.role = Role.USER;
+
+  const userWithPremiumRole = new User(
+    "John",
+    "Doe",
+    "john.doe@email.com",
+    "password"
+  );
+  userWithPremiumRole.role = Role.PREMIUM;
+
+  const url = "https://example.com";
+  const frequency = Frequency.ONE_HOUR;
+  const name = "dummyName";
+  const headers = '[{"property":"content-type","value":"application/json"}]';
+  const isActive = false;
+  const allErrorsEnabledEmail = true;
+  const allErrorsEnabledPush = false;
+  const customEmailErrors: number[] = [];
+  const customPushErrors: number[] = [];
+
+  const checkForErrorsAndCreateRequestForUser = () => {
+    RequestSettingService.checkForErrorsAndCreateRequest(
+      url,
+      frequency,
+      name,
+      headers,
+      isActive,
+      allErrorsEnabledEmail,
+      allErrorsEnabledPush,
+      customEmailErrors,
+      customPushErrors,
+      userWithUserRole
+    );
+  };
+
+  const checkForErrorsAndCreateRequestForPremium = () => {
+    RequestSettingService.checkForErrorsAndCreateRequest(
+      url,
+      Frequency.ONE_MINUTE,
+      name,
+      headers,
+      isActive,
+      allErrorsEnabledEmail,
+      allErrorsEnabledPush,
+      [400],
+      [400],
+      userWithPremiumRole
+    );
+  };
+
+  let setPushAlertsSpy: jest.SpyInstance<Promise<void>>;
+  let setEmailAlertsSpy: jest.SpyInstance<Promise<void>>;
+  let checkIfHeadersAreRightFormattedSpy: jest.SpyInstance<void>;
+  let checkIfNonPremiumUserTryToUsePremiumFrequencySpy: jest.SpyInstance<void>;
+  let checkIfNonPremiumUserTryToUseCustomErrorSpy: jest.SpyInstance<void>;
+  let createRequestSettingSpy: jest.SpyInstance<Promise<RequestSetting>>;
+
+  beforeAll(async () => {
+    await getDatabase();
+    await initializeRepositories();
+  });
+
+  beforeEach(async () => {
+    await truncateAllTables();
+    setPushAlertsSpy = setPushAlerts();
+    setEmailAlertsSpy = setEmailAlerts();
+    checkIfHeadersAreRightFormattedSpy = checkIfHeadersAreRightFormatted();
+    checkIfNonPremiumUserTryToUsePremiumFrequencySpy =
+      checkIfNonPremiumUserTryToUsePremiumFrequency();
+    checkIfNonPremiumUserTryToUseCustomErrorSpy =
+      checkIfNonPremiumUserTryToUseCustomError();
+    createRequestSettingSpy = createRequestSetting();
+  });
+
+  afterAll(async () => {
+    await closeConnection();
+  });
+
   describe("checkForErrorsAndCreateRequest", () => {
-    it("calls checkIfHeadersAreRightFormatted once", () => {});
-    it("calls checkIfNonPremiumUserTryToUsePremiumFrequency once", () => {});
-    it("calls checkIfNonPremiumUserTryToUseCustomError once", () => {});
+    it("calls checkIfHeadersAreRightFormatted once", async () => {
+      checkForErrorsAndCreateRequestForUser();
+      expect(checkIfHeadersAreRightFormattedSpy).toHaveBeenCalledTimes(1);
+    });
+    it("calls checkIfNonPremiumUserTryToUsePremiumFrequency once", () => {
+      checkForErrorsAndCreateRequestForUser();
+      expect(
+        checkIfNonPremiumUserTryToUsePremiumFrequencySpy
+      ).toHaveBeenCalledTimes(1);
+    });
+    it("calls checkIfNonPremiumUserTryToUseCustomError once", () => {
+      checkForErrorsAndCreateRequestForUser();
+      expect(checkIfNonPremiumUserTryToUseCustomErrorSpy).toHaveBeenCalledTimes(
+        1
+      );
+    });
     describe("if there's no validation error", () => {
-      it("calls createRequestSetting once", () => {});
-      it("calls setPushAlerts once", () => {}); // MOCKER
-      it("calls setEmailAlerts once", () => {}); // MOCKER
+      it("calls createRequestSetting once", () => {
+        checkForErrorsAndCreateRequestForUser();
+        expect(createRequestSettingSpy).toHaveBeenCalledTimes(1);
+      });
+      it("calls setPushAlerts once", () => {
+        checkForErrorsAndCreateRequestForUser();
+        expect(setPushAlertsSpy).toHaveBeenCalledTimes(1);
+      });
+      it("calls setEmailAlerts once", () => {
+        checkForErrorsAndCreateRequestForUser();
+        expect(setEmailAlertsSpy).toHaveBeenCalledTimes(1);
+      });
       it("returns RequestSetting object", () => {});
     });
   });
